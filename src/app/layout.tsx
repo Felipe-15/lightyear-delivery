aa35defbed07e3e118254b7eaf96581aa6333a86
Felipe Souza <felipesouza036@gmail.com>
2025-03-24 13:15:09 -0300

    fix (Home): Add suspense to avoid errors while access URL with useSearchParams

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
+import "./globals.css";
 
 const interFont = Inter({
   subsets: ["latin"],
